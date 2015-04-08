2922d12edc25e7f3ca6472968bc139c6d9746d95
jiajingchun <chinajiajingchun@gmail.com>
2015-04-08 16:42:25 +0800

    Update the file structure

--- app/src/main/java/com/jjc/tab/MainActivity.java
+++ app/src/main/java/com/jjc/tab/MainActivity.java
@@ -1,44 +1,83 @@
+/*
+ * Copyright 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.jjc.tab;
 
+import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+/**
+ * The launchpad activity for this sample project. This activity launches other activities that
+ * demonstrate implementations of common TabHost.
+ */
 import com.jjc.tab.FragmentTransaction.FragmentTransactionActivity;
 
+public class MainActivity extends ListActivity {
 
-public class MainActivity extends ActionBarActivity {
+    /**
+     * This class describes an individual sample (the sample title, and the activity class that
+     * demonstrates this sample).
+     */
+    private class Sample {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
+
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
+        }
+
+        @Override
+        public String toString() {
+            return title.toString();
+        }
+    }
+
+    /**
+     * The collection of all samples in the app. This gets instantiated in {@link
+     * #onCreate(android.os.Bundle)} because the {@link Sample} constructor needs access to {@link
+     * android.content.res.Resources}.
+     */
+    private static Sample[] mSamples;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(MainActivity.this, FragmentTransactionActivity.class));
+        mSamples = new Sample[]{
+                new Sample(R.string.title_transaction, FragmentTransactionActivity.class),
+        };
+
+        setListAdapter(new ArrayAdapter<Sample>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                mSamples));
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // Launch the sample associated with this list position.
+        startActivity(new Intent(MainActivity.this, mSamples[position].activityClass));
     }
 }
